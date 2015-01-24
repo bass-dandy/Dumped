50c34a5eb0a763593c1a8d243c2b2718ca98fe95
Christian Dinh <ctdinh93@gmail.com>
2015-01-24 03:23:01 -0600

    Add piickup-able plunger

---
+++ Assets/Scripts/Plunger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Plunger : MonoBehaviour {
+
+	public GameObject playerPlunger;
+
+	void ShowPrompt() {
+	
+	}
+	
+	void Activate() {
+		playerPlunger.SetActive(true);
+		Destroy(gameObject);
+	}
+}
