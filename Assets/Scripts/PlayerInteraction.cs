a7a2b23a0ff37a657744ff6b74698c543265ea92
Christian Dinh <ctdinh93@gmail.com>
2015-01-24 02:53:16 -0600

    Add Jame's pretty toilet and some animations, add raycasting so things are interactable

---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInteraction : MonoBehaviour {
+	
+	public float radius;
+	
+	void Update () {
+		RaycastHit hit;
+		if(Physics.Raycast(transform.position, transform.forward, out hit, radius)) {
+			if(hit.transform.tag == "Interactable") {
+				hit.transform.SendMessage("ShowPrompt");
+				if(Input.GetKeyDown(KeyCode.E)) {
+					hit.transform.SendMessage("Activate");
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/ToiletHandle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToiletHandle : MonoBehaviour {
+
+	public Material idle;
+	public Material highlight;
+
+	void ShowPrompt() {
+		renderer.material = highlight;
+	}
+
+	void Activate() {
+		transform.parent.parent.animation.Play();
+	}
+	
+	void Update() {
+		renderer.material = idle;
+	}
+}
