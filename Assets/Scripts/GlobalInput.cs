afdbb68f9b05cc892dad8cd9e41d9555f5350b80
Christian Dinh <ctdinh93@gmail.com>
2015-01-24 12:36:19 -0600

    Make plunger throwable, trap cursor

---
+++ Assets/Scripts/GlobalInput.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalInput : MonoBehaviour {
+
+	void Start () {
+		Screen.lockCursor = true;
+	}
+	
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.Q)) {
+			Debug.Break();
+		}
+		Screen.lockCursor = true;
+	}
+}
--- Assets/Scripts/PlayerPlunger.cs
+++ Assets/Scripts/PlayerPlunger.cs
@@ -3,8 +3,17 @@ using System.Collections;
 
 public class PlayerPlunger : MonoBehaviour {
 
+	public GameObject worldPlunger;
+	public float throwForce;
+
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.Mouse0))
 			animation.Play();
+			
+		if(Input.GetKeyDown (KeyCode.Mouse1)) {
+			GameObject p = Instantiate (worldPlunger, transform.position, transform.rotation) as GameObject;
+			p.rigidbody.velocity = transform.parent.gameObject.transform.forward * throwForce;
+			gameObject.SetActive(false);
+		}
 	}
 }
