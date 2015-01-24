a73c9920f87d88da22b94f105c3ef7eaf6c9e572
Christian Dinh <ctdinh93@gmail.com>
2015-01-23 21:22:43 -0600

    Add first person player movement controls and debug scene

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	
+	public float speed;
+	public Transform head;
+	
+	void Update () {
+		// Forward/backward component
+		if(Input.GetKey(KeyCode.W)) {
+			rigidbody.position += transform.forward * speed * Time.deltaTime;
+		}
+		if(Input.GetKey(KeyCode.S)) {
+			rigidbody.position -= transform.forward * speed * Time.deltaTime;
+		}
+		
+		// Left/right component
+		if(Input.GetKey(KeyCode.A)) {
+			rigidbody.position -= transform.right * speed * Time.deltaTime;
+		}
+		else if(Input.GetKey(KeyCode.D)) {
+			rigidbody.position += transform.right * speed * Time.deltaTime;
+		}
+	}
+}
